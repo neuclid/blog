78d85d79a42488b2f41fb11e372f47e1cc885c72
neuclid <iamneuclid@gmail.com>
2018-02-05 01:48:49 +0530

    Changing SITEURL to HTTPS to see if CSS works

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://neuclid.github.io/blog'
+SITEURL = 'https://neuclid.github.io/blog'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
